b822eeb46a3934f180aeea8d421522a9c8cf89d8
johnabass <johnbass1017@yahoo.com>
2020-07-20 13:24:59 -0700

    Upgraded mapstructure to the latest

---
+++ doc.go
@@ -0,0 +1,12 @@
+// Package arrange enhances uber/fx with couple of features:
+//
+// Simple support for unmarshaled components
+//
+// The various UnmarshalXXX functions are general-purpose constructors
+// for unmarshaled objects.  They work with fx.Provide and fx.Annotated.
+//
+// Conditional options
+//
+// Arrange exposes a simple API for conditional inclusion of options for
+// fx.New.
+package arrange

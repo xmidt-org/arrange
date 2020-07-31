77e3cac35fc8adad640b9c6c37cbe7ad850b9190
johnabass <johnbass1017@yahoo.com>
2020-07-30 21:39:43 -0700

    pruned dead code; better comments

--- arrangehttp/server.go
+++ arrangehttp/server.go
@@ -11,10 +11,23 @@ import (
 	"go.uber.org/fx"
 )
 
+// ServerFactory is the creation strategy for both an http.Server and the
+// particular listener used for the accept loop.  This interface is implemented
+// by any unmarshaled struct which hold server configuration fields.
 type ServerFactory interface {
+	// NewServer is responsible for creating an http.Server using whatever
+	// information was unmarshaled into this instance.
+	//
+	// The Listen strategy is used to create the net.Listener for the server's
+	// accept loop.  Since various parts of this listener can be driven by configuration,
+	// for example the connection keep alive, this method must supply a non-nil Listen.
+	// See ListenerFactory for a convenient way to provide a Listen closure.
 	NewServer() (*http.Server, Listen, error)
 }
 
+// ServerConfig is the built-in ServerFactory implementation for this package.
+// This struct can be unmarshaled via Viper, thus allowing an http.Server to
+// be bootstrapped from external configuration.
 type ServerConfig struct {
 	Network           string
 	Address           string
@@ -50,10 +63,19 @@ func (sc ServerConfig) NewServer() (server *http.Server, l Listen, err error) {
 	return
 }
 
+// ServerOption is a functional option that is allowed to mutate an http.Server
+// prior to binding it to an uber/fx App
 type ServerOption func(*http.Server) error
 
+// RouterOption is a functional option that can mutate a mux.Router prior to
+// it being returned as a component in an uber/fx App.  A RouterOption does not
+// participate in dependency injection; rather, its purpose is to establish
+// custom settings that do not depend on components, such as any global middleware
+// or settings such as SkipClean.
 type RouterOption func(*mux.Router) error
 
+// ServerIn describes the set of dependencies for creating a mux.Router and,
+// by extension, an http.Server.
 type ServerIn struct {
 	arrange.ProvideIn
 
@@ -61,6 +83,8 @@ type ServerIn struct {
 	Shutdowner fx.Shutdowner
 }
 
+// S is a Fluent Builder for unmarshaling an http.Server.  This type is typically
+// created with the Server function.
 type S struct {
 	so        []ServerOption
 	ro        []RouterOption

f64b9dd0ed97961dab5dc7e47c8c40a6da9868db
johnabass <johnbass1017@yahoo.com>
2020-10-19 13:52:35 -0700

    added injectable headers into ServerConfig

--- arrangehttp/server_test.go
+++ arrangehttp/server_test.go
@@ -5,6 +5,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"strings"
@@ -36,13 +38,14 @@ type simpleServerFactory struct {
 	returnErr error
 }
 
-func (ssf simpleServerFactory) NewServer() (*http.Server, error) {
+func (ssf simpleServerFactory) NewServer(http.Handler) (*http.Server, error) {
 	if ssf.returnErr != nil {
 		return nil, ssf.returnErr
 	}
 
 	return &http.Server{
 		Addr: ssf.Address,
+		// this factory does not set a handler, forcing the infrastructure to set it
 	}, nil
 }
 
@@ -65,9 +68,10 @@ func testServerConfigBasic(t *testing.T) {
 		address = make(chan net.Addr, 1)
 	)
 
-	server, err := serverConfig.NewServer()
+	server, err := serverConfig.NewServer(router)
 	require.NoError(err)
 	require.NotNil(server)
+	assert.Equal(router, server.Handler)
 
 	assert.Equal(15*time.Second, server.ReadTimeout)
 	assert.Equal(27*time.Minute, server.ReadHeaderTimeout)
@@ -78,7 +82,6 @@ func testServerConfigBasic(t *testing.T) {
 	// check that this is a functioning server
 	lf := NewListenerChain(CaptureListenAddress(address)).
 		Factory(DefaultListenerFactory{})
-	server.Handler = router
 	require.NoError(
 		ServerOnStart(server, lf)(context.Background()),
 	)
@@ -127,9 +130,10 @@ func testServerConfigTLS(t *testing.T) {
 		address = make(chan net.Addr, 1)
 	)
 
-	server, err := serverConfig.NewServer()
+	server, err := serverConfig.NewServer(router)
 	require.NoError(err)
 	require.NotNil(server)
+	assert.Equal(router, server.Handler)
 
 	assert.Equal(72*time.Second, server.ReadTimeout)
 	assert.Equal(109*time.Minute, server.ReadHeaderTimeout)
@@ -140,7 +144,6 @@ func testServerConfigTLS(t *testing.T) {
 	// check that this is a functioning server
 	lf := NewListenerChain(CaptureListenAddress(address)).
 		Factory(DefaultListenerFactory{})
-	server.Handler = router
 	require.NoError(
 		ServerOnStart(server, lf)(context.Background()),
 	)
@@ -162,9 +165,56 @@ func testServerConfigTLS(t *testing.T) {
 	}
 }
 
+func testServerConfigHeader(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		serverConfig = ServerConfig{
+			Header: http.Header{
+				"test1": {"true"},
+				"test2": {"1", "2"},
+			},
+		}
+
+		router  = mux.NewRouter()
+		address = make(chan net.Addr, 1)
+	)
+
+	server, err := serverConfig.NewServer(router)
+	require.NoError(err)
+	require.NotNil(server)
+	assert.NotNil(server.Handler)
+
+	// check that this is a functioning server
+	lf := NewListenerChain(CaptureListenAddress(address)).
+		Factory(DefaultListenerFactory{})
+	require.NoError(
+		ServerOnStart(server, lf)(context.Background()),
+	)
+
+	defer server.Close()
+
+	select {
+	case listenAddress := <-address:
+		response, err := http.Get("http://" + listenAddress.String())
+		require.NoError(err)
+		require.NotNil(response)
+		defer response.Body.Close()
+		io.Copy(ioutil.Discard, response.Body)
+
+		assert.Equal([]string{"true"}, response.Header["Test1"])
+		assert.Equal([]string{"1", "2"}, response.Header["Test2"])
+
+	case <-time.After(2 * time.Second):
+		assert.Fail("No captured listen address")
+	}
+}
+
 func TestServerConfig(t *testing.T) {
 	t.Run("Basic", testServerConfigBasic)
 	t.Run("TLS", testServerConfigTLS)
+	t.Run("Header", testServerConfigHeader)
 }
 
 func testServerInjectError(t *testing.T) {

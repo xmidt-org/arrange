36dbb698a453d5c347043f9ddfc3cf45454a82e4
johnabass <johnbass1017@yahoo.com>
2020-10-26 00:06:28 -0700

    basic arrangepprof package

---
+++ arrangepprof/doc.go
@@ -0,0 +1,6 @@
+/*
+Package arrangepprof enables pprof functionality bound to an fx.App instance.
+This package also exposes utilities for configuring the various net/http/pprof
+handlers on a gorilla/mux.Router.
+*/
+package arrangepprof

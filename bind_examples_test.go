2704803d49a21012d9ae874ba292beb40b177a9d
johnabass <johnbass1017@yahoo.com>
2021-05-06 14:50:03 -0700

    expanded example to demonstrate that a mixture of components and bindings is allowed

--- bind_examples_test.go
+++ bind_examples_test.go
@@ -13,23 +13,35 @@ func ExampleBind() {
 
 	fx.New(
 		DiscardLogger(),
+		fx.Provide(
+			func() []byte {
+				// an example of a component created in the normal fashion
+				// we'll use this as a suffix, and it needs to be a different
+				// type than string since we're binding that type in With
+				return []byte("!!!")
+			},
+		),
 		Bind{
 			func(v string) {
 				// v is supplied by With
 				fmt.Println(v)
 			},
-			func(v string) *bytes.Buffer {
+			func(v string, suffix []byte) *bytes.Buffer {
 				// we can use v to construct even more components
-				return bytes.NewBufferString(v)
+				// we can also refer to other components present in the enclosing app
+				b := bytes.NewBufferString(v)
+				b.Write(suffix)
+				return b
 			},
 			func(b *bytes.Buffer) {
-				// ... and we can refer to those components as desired elsewhere
+				// ... and we can refer to components created inside this block as desired
 				fmt.Println(b.String())
 			},
 		}.With(info),
 		fx.Provide(
-			func(unused *bytes.Buffer) (int, error) {
+			func(b *bytes.Buffer) (int, error) {
 				// components created inside Bind.With are normal components
+				fmt.Println(b)
 				return 123, nil
 			},
 		),
@@ -40,6 +52,7 @@ func ExampleBind() {
 
 	// Output:
 	// here is a lovely little informational string
-	// here is a lovely little informational string
+	// here is a lovely little informational string!!!
+	// here is a lovely little informational string!!!
 	// 123
 }

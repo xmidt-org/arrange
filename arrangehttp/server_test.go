8ebb1c3887a11884d6cbab6c01487bcdf87716d2
johnabass <johnbass1017@yahoo.com>
2023-06-07 14:21:07 -0700

    chore: initial tests for NewServer

--- arrangehttp/server_test.go
+++ arrangehttp/server_test.go
@@ -1,9 +1,12 @@
 package arrangehttp
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxtest"
 )
 
 type ServerSuite struct {
@@ -11,6 +14,70 @@ type ServerSuite struct {
 }
 
 func (suite *ServerSuite) TestApplyServerOptions() {
+	var (
+		expectedServer = new(http.Server)
+		actualServer   *http.Server
+
+		mock0 = new(mockServerOption)
+		mock1 = new(mockServerOption)
+	)
+
+	mock0.ExpectApply(expectedServer).Return(nil)
+	mock1.ExpectApply(expectedServer).Return(nil)
+
+	app := fxtest.New(
+		suite.T(),
+		fx.Provide(
+			func() *http.Server {
+				return expectedServer
+			},
+			fx.Annotate(
+				func() ServerOption { return mock0 },
+				fx.ResultTags(`group:"options"`),
+			),
+			fx.Annotate(
+				func() ServerOption { return mock1 },
+				fx.ResultTags(`group:"options"`),
+			),
+		),
+		fx.Decorate(
+			fx.Annotate(
+				ApplyServerOptions,
+				fx.ParamTags("", `group:"options"`),
+			),
+		),
+		fx.Populate(&actualServer),
+	)
+
+	app.RequireStart()
+	app.RequireStop()
+
+	suite.Same(expectedServer, actualServer)
+	mock0.AssertExpectations(suite.T())
+	mock1.AssertExpectations(suite.T())
+}
+
+func (suite *ServerSuite) TestNewServer() {
+	var server *http.Server
+	app := fxtest.New(
+		suite.T(),
+		fx.Supply(ServerConfig{
+			Address: ":1234",
+		}),
+		fx.Provide(
+			fx.Annotate(
+				NewServer,
+				fx.ParamTags("", `optional:"true"`),
+			),
+		),
+		fx.Populate(&server),
+	)
+
+	app.RequireStart()
+	app.RequireStop()
+
+	suite.Require().NotNil(server)
+	suite.Equal(":1234", server.Addr)
 }
 
 func TestServer(t *testing.T) {

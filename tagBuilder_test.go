b94dd6a9b855d3596143da95baec4b0ec8b930d5
johnabass <johnbass1017@yahoo.com>
2023-06-16 18:37:56 -0700

    chore: docs and tests

--- tagBuilder_test.go
+++ tagBuilder_test.go
@@ -2,6 +2,7 @@ package arrange
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -50,6 +51,84 @@ func (suite *TagBuilderSuite) TestParamTags() {
 	app.RequireStart()
 	app.RequireStop()
 	suite.NotNil(buffer)
+}
+
+func (suite *TagBuilderSuite) TestResultTags() {
+	type populate struct {
+		fx.In
+		Named   *bytes.Buffer   `name:"named"`
+		Buffers []*bytes.Buffer `group:"buffers"`
+	}
+
+	var p populate
+
+	app := fxtest.New(
+		suite.T(),
+		fx.Provide(
+			fx.Annotate(
+				func() *bytes.Buffer { return new(bytes.Buffer) },
+				Tags().Name("named").ResultTags(),
+			),
+			fx.Annotate(
+				func() *bytes.Buffer { return new(bytes.Buffer) },
+				Tags().Group("buffers").ResultTags(),
+			),
+			fx.Annotate(
+				func() *bytes.Buffer { return new(bytes.Buffer) },
+				Tags().Group("buffers").ResultTags(),
+			),
+		),
+		fx.Invoke(
+			func(in populate) {
+				p = in
+			},
+		),
+	)
+
+	app.RequireStart()
+	app.RequireStop()
+	suite.NotNil(p.Named)
+	suite.Len(p.Buffers, 2)
+}
+
+func (suite *TagBuilderSuite) assertStructTag(st reflect.StructTag, expectedName, expectedGroup string, optional bool) {
+	v, ok := st.Lookup("optional")
+	if optional {
+		suite.Equal("true", v)
+	} else {
+		suite.False(ok)
+	}
+
+	v, ok = st.Lookup("name")
+	if len(expectedName) > 0 {
+		suite.Equal(expectedName, v)
+	} else {
+		suite.False(ok)
+	}
+
+	v, ok = st.Lookup("group")
+	if len(expectedGroup) > 0 {
+		suite.Equal(expectedGroup, v)
+	} else {
+		suite.False(ok)
+	}
+}
+
+func (suite *TagBuilderSuite) TestStructTags() {
+	tags := Tags().
+		Optional().
+		Skip().
+		Group("group").
+		Name("name").
+		OptionalName("optional").
+		StructTags()
+
+	suite.Require().Len(tags, 5)
+	suite.assertStructTag(tags[0], "", "", true)
+	suite.Empty(string(tags[1]))
+	suite.assertStructTag(tags[2], "", "group", false)
+	suite.assertStructTag(tags[3], "name", "", false)
+	suite.assertStructTag(tags[4], "optional", "", true)
 }
 
 func TestTagBuilder(t *testing.T) {

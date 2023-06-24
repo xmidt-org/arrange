6af4643c7b45b02e59ef4f8bba0665babc94c714
johnabass <johnbass1017@yahoo.com>
2023-06-24 13:47:39 -0700

    added generic middleware

---
+++ arrangehttp/middleware.go
@@ -0,0 +1,16 @@
+package arrangehttp
+
+// Middleware is the underlying type for decorators.
+type Middleware[T any] interface {
+	~func(T) T
+}
+
+// ApplyMiddleware handles decorating a target type T.  Middleware
+// executes in the order declared to this function.
+func ApplyMiddleware[T any, M Middleware[T]](t T, m ...M) T {
+	for i := len(m) - 1; i >= 0; i-- {
+		t = m[i](t)
+	}
+
+	return t
+}

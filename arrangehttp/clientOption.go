16af5667758ed12fafef23a03f4a2d5a7eeb90a8
johnabass <johnbass1017@yahoo.com>
2023-06-24 13:52:23 -0700

    chore: migrated Decorate into ApplyMiddleware

--- arrangehttp/clientOption.go
+++ arrangehttp/clientOption.go
@@ -6,16 +6,11 @@ import (
 	"github.com/xmidt-org/arrange/internal/arrangereflect"
 )
 
-// ClientMiddlewareFunc is the underlying type for round tripper decoration.
-type ClientMiddlewareFunc interface {
-	~func(http.RoundTripper) http.RoundTripper
-}
-
 // ClientMiddleware returns a ClientOption that applies the given middleware
 // to the Transport (http.RoundTripper).
-func ClientMiddleware[M ClientMiddlewareFunc](fns ...M) Option[http.Client] {
+func ClientMiddleware[M Middleware[http.RoundTripper]](fns ...M) Option[http.Client] {
 	return AsOption[http.Client](func(c *http.Client) {
-		c.Transport = arrangereflect.Decorate(
+		c.Transport = ApplyMiddleware(
 			arrangereflect.Safe[http.RoundTripper](c.Transport, http.DefaultTransport),
 			fns...,
 		)
